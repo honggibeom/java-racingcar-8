37cb2dc3fdb35075916c4bd392cc7dd2b48bfdb4
honggibeom <90910169+honggibeom@users.noreply.github.com>
2025-10-26 10:49:28 +0900

    refactor: RacingService객체를 controller에 통합
    기존 RacingSercvice 객체는 view를 선언하여 게임 결과를 출력했습니다. 하지만 이것은 의존성 방향이 역전된 구조이기 때문이기 때문에 sercvice를 삭제하고 그 기능을 컨트롤러에 통합하였습니다.

--- src/main/java/racingcar/Controller/RacingController.java
+++ src/main/java/racingcar/Controller/RacingController.java
@@ -3,13 +3,13 @@ package racingcar.Controller;
 import racingcar.Model.CarNameValidator;
 import racingcar.Model.NumberValidator;
 import racingcar.Model.Racing;
-import racingcar.Service.RacingService;
 import racingcar.View.InputView;
 import racingcar.View.RacingView;
 
 public class RacingController {
     InputView inputView;
     RacingView racingView;
+    Racing racing;
 
     public RacingController(InputView inputView, RacingView racingView) {
         this.inputView = inputView;
@@ -23,9 +23,15 @@ public class RacingController {
         String round = inputView.inputRoundNumber();
         NumberValidator.validateNumber(round);
 
-        Racing racing = new Racing(cars);
-        RacingService racingService = new RacingService(racing, this.racingView);
+        this.racing = new Racing(cars);
+        playRounds(Integer.parseInt(round));
+    }
 
-        racingService.playRounds(Integer.parseInt(round));
+    public void playRounds(int roundCount) {
+        for (int i = 0; i < roundCount; i++) {
+            racing.startRound();
+            racingView.printProgress(racing.getCars());
+        }
+        racingView.printResult(racing.getWinners());
     }
 }
