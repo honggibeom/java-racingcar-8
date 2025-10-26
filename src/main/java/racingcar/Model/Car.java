0e7dacacfbe868cf63c68e77b6700b1552dc1fdc
honggibeom <90910169+honggibeom@users.noreply.github.com>
2025-10-26 10:02:30 +0900

    feat: 자동차 객체 구현

---
+++ src/main/java/racingcar/Model/Car.java
@@ -0,0 +1,39 @@
+package racingcar.Model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car implements Comparable<Car> {
+    private final StringBuilder state;
+    private final String name;
+    int pos;
+
+    public Car(String name) {
+        this.name = name;
+        state = new StringBuilder();
+        this.pos = 0;
+    }
+
+    public void move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            this.state.append("-");
+            pos++;
+        }
+    }
+
+    @Override
+    public int compareTo(Car car) {
+        return Integer.compare(car.pos, this.pos);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public StringBuilder getState(){
+        return this.state;
+    }
+
+    public int getPos() {
+        return this.pos;
+    }
+}
